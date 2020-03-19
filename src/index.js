cbd188fe66f97ba30042273543f14ef3d218161c
Vladislav Zheromsky <vladislavzheromsky@gmail.com>
2020-03-19 21:07:56 +0300

    feat: implement task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	if (str.length % 2) {
+		return false;
+	}
+
+	let brOpen = [], brClose = [];
+	bracketsConfig.forEach(config => {
+		config.forEach((element, index) => {
+			if (index % 2 === 0) {
+				brOpen.push(element);
+			} else {
+				brClose.push(element);
+			}
+		});
+	});
+
+	for (let i = 0; i < bracketsConfig.length; i++) {
+        while (true) {
+			let pair = brOpen[i] + brClose[i];
+            if (str.includes(pair)) {
+                str = str.replace(pair, "");
+                i = 0;
+            } else { 
+				break; 
+			}
+        }
+    }
+
+    return str.length === 0;
 }
